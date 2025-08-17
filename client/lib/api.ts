957c82c180e6b9613be0e61e2ecc5dce486247ee
Ahmad Ashfaq <spoiledwit@gmail.com>
2025-08-17 17:02:24 +0500

    feat: final

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://fileauthai-admin.credminds.com/api"
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://admin-engine2359.jojen.co.uk/api"
 });
 
 api.interceptors.request.use((config) => {
